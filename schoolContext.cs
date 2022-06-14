e60f34a04a32e2509ed6729a441e5b347be83a27
Александр <alexander9106@mail.ru>
2022-06-14 19:51:30 +0900

    Строка подключения

--- schoolContext.cs
+++ schoolContext.cs
@@ -28,8 +28,7 @@ namespace School_tametable
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlite(" Data Source= C:\\\\\\\\Users\\\\\\\\Alexa\\\\\\\\OneDrive\\\\\\\\Program\\\\\\\\School_tametable\\\\\\\\DB\\\\\\\\school.db ");
+               optionsBuilder.UseSqlite(" Data Source= C:\\Users\\Alexa\\OneDrive\\Program\\School_tametable\\DB\\school.db ");
             }
         }
 
